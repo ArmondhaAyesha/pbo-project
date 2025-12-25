c1a1a874c6bae88345f5487f2f5c7253a5980d39
ArmondhaAyesha <ayeshamondha@gmail.com>
2025-12-26 00:46:30 +0700

    Add files via upload

---
+++ MataKuliah.java
@@ -0,0 +1,22 @@
+public class MataKuliah {
+
+    private String kodeMatkul;
+    private String namaMatkul;
+    private int sks;
+    private Dosen dosenPengampu;
+
+    public MataKuliah(String kodeMatkul, String namaMatkul, int sks, Dosen dosenPengampu) {
+        this.kodeMatkul = kodeMatkul;
+        this.namaMatkul = namaMatkul;
+        this.sks = sks;
+        this.dosenPengampu = dosenPengampu;
+    }
+
+    public String getNamaMatkul() {
+        return namaMatkul;
+    }
+
+    public String getNamaDosen() {
+        return dosenPengampu.getNama();
+    }
+}
