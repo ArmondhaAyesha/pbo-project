58c4c950af8e898e7dc235f324a101ae4c4ac1ef
Renjiro221 <2400018266@webmail.uad.ac.id>
2026-01-08 23:50:08 +0700

    Update Mahasiswa.java

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -1,43 +1,17 @@
 public class Mahasiswa extends Identitas {
+    private String kelas;
 
-    private String kelas;  
-    private Nilai nilai;    
-
-    public Mahasiswa(String nim, String nama, String email, String kelas, Nilai nilai) {
-        super(nim, nama, email);    
+    public Mahasiswa(String nim, String nama, String kelas) {
+        super(nim, nama);
         this.kelas = kelas;
-        this.nilai = nilai;
+    }
+
+    @Override
+    public String getJenis() {
+        return "Mahasiswa";
     }
 
     public String getKelas() {
         return kelas;
     }
-
-    public void setKelas(String kelas) {
-        this.kelas = kelas;
-    }
-
-    public Nilai getNilai() {
-        return nilai;
-    }
-
-    public void setNilai(Nilai nilai) {
-        this.nilai = nilai;
-    }
-
-    public void tampilMahasiswa() {
-        super.tampilIdentitas(); 
-        System.out.println("Kelas : " + kelas);
-
-        if (nilai != null) {
-            System.out.println("UTS   : " + nilai.getUts());
-            System.out.println("UAS   : " + nilai.getUas());
-            System.out.println("Tugas : " + nilai.getTugas());
-            System.out.println("Rata-rata : " + nilai.hitungRataRata());
-            System.out.println("Status    : " + nilai.statusKelulusan());
-        }
-    }
-
 }
-
-
