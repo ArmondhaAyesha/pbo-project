626d867e11459a1d4881901fd25de56e6f5eee3b
AmarillisAnnisa <2400018285@webmail.uad.ac.id>
2026-01-15 00:36:19 +0700

    Update Nilai.java
    update baru nilai

--- Nilai.java
+++ Nilai.java
@@ -1,7 +1,7 @@
 public class Nilai implements Penilaian {
 
-    private Mahasiswa mahasiswa;
-    private MataKuliah mataKuliah;
+    private Mahasiswa mahasiswa;   // asosiasi
+    private MataKuliah mataKuliah; // agregasi
     private double uts, uas, tugas;
 
     public Nilai(Mahasiswa mahasiswa, MataKuliah mataKuliah,
@@ -13,11 +13,18 @@ public class Nilai implements Penilaian {
         this.tugas = tugas;
     }
 
+    // overriding
     @Override
     public double hitungRata() {
         return (uts + uas + tugas) / 3;
     }
 
+    // overloading
+    public double hitungRata(double uts, double uas, double tugas) {
+        return (uts + uas + tugas) / 3;
+    }
+
+    // overriding
     @Override
     public String getStatus() {
         return hitungRata() >= 70 ? "LULUS" : "TIDAK LULUS";
